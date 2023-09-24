17b436e87b36c91a83a8752431e1bfa3a3f91584
Syed Bilal Abbas <abbasb000@gmail.com>
2023-09-25 04:04:45 +0500

    changed format to: YYYY/YYYY-MM/YYYY-MM-DD

--- main.py
+++ main.py
@@ -2,23 +2,37 @@ import datetime
 import os
 
 
-def create_folders_by_date(start_date, end_date):
-    """Creates folders with the format YYYYMMDD from the start date to the end date.
+def generate_folders(start_date, end_date, base_dir):
+    """Generates a hierarchy of folders from start_date to end_date, starting with a year folder, then year-month
+    folders, and finally day folders.
 
   Args:
     start_date: A datetime.date object representing the start date.
     end_date: A datetime.date object representing the end date.
+    base_dir: The base directory where the folders will be created.
   """
 
-    for date in range(start_date.toordinal(), end_date.toordinal() + 1):
-        folder_name = datetime.date.fromordinal(date).strftime("%Y%m%d")
-        folder_path = os.path.join(os.getcwd(), "Folders/" + folder_name)
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
+    while start_date <= end_date:
+        year_folder = os.path.join(base_dir, str(start_date.year))
+        year_month_folder = os.path.join(year_folder, f"{start_date.year}-{start_date.month:02d}")
+        year_month_day_folder = os.path.join(year_month_folder,
+                                             f"{start_date.year}-{start_date.month:02d}-{start_date.day:02d}")
+        # day_folder = os.path.join(year_month_folder, str(start_date.day).zfill(2))
+
+        # Create the folders if they don't exist.
+        if not os.path.exists(year_folder):
+            os.makedirs(year_folder)
+        if not os.path.exists(year_month_folder):
+            os.makedirs(year_month_folder)
+        if not os.path.exists(year_month_day_folder):
+            os.makedirs(year_month_day_folder)
+
+        start_date += datetime.timedelta(days=1)
 
 
-if __name__ == "__main__":
-    my_start_date = datetime.date.today()
-    my_end_date = datetime.date(2023, 12, 31)
+# Get the start and end dates.
+my_start_date = datetime.date(2023, 1, 1)
+my_end_date = datetime.date(2023, 12, 31)
 
-    create_folders_by_date(my_start_date, my_end_date)
+# Create the folders.
+generate_folders(my_start_date, my_end_date, "")
